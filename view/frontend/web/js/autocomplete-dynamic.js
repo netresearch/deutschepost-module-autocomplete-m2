a4644a18180eb07d1027ac9a4885d3caf337f867
Paul Siedler <paul.siedler@netresearch.de>
2020-04-14 23:55:08 +0200

    PDTWO-111: fix parameter order

--- view/frontend/web/js/autocomplete-dynamic.js
+++ view/frontend/web/js/autocomplete-dynamic.js
@@ -59,8 +59,8 @@ define([
                         try {
                             Autocomplete.init(
                                 formInputs.street,
-                                formInputs.postalCode,
                                 formInputs.city,
+                                formInputs.postalCode,
                                 formInputs.country,
                                 deCountryId,
                                 token
